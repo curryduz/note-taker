320c2c6d470b5333abd95766e22cc18378217aa4
T. Curry <74472798+curryduz@users.noreply.github.com>
2021-03-07 15:29:01 -0600

    server and html routes added

---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express()
+const PORT = process.env.PORT || 3001 
+app.use(express.static("public"))
+
+app.use(express.urlencoded({ extended:true}))  
+app.use(express.json())
+
+app.use(require("./routes/htmlroutes"))
+
+app.listen(PORT, function(){
+    console.log("app is listening on PORT", PORT)
+})
